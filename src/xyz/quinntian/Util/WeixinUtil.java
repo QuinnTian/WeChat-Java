032e42dfdce6abbaf69a1bc49e49bb6e5d868c1d
quinntian <530479731@qq.com>
2018-04-06 01:29:03 +0800

    修改WeixinUtil的内容，还有两个Jar package

--- src/xyz/quinntian/Util/WeixinUtil.java
+++ src/xyz/quinntian/Util/WeixinUtil.java
@@ -1,5 +1,56 @@
 package xyz.quinntian.Util;
 
-public class WeixinUtil {
+import java.io.IOException;
+import java.text.ParseException;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import net.sf.json.JSONObject;
+
+public class WeixinUtil {
+	private static final String APPID = "wx1e2f189602bb3de7";
+	private static final String APPSECRET = "69b2255a1640e16cca28c2c4500e0588";
+	/**
+	 * 
+	 * @param url
+	 * @return
+	 * @throws ParseException
+	 * @throws IOException
+	 */
+	public static JSONObject doGetStr(String url) throws ParseException, IOException{
+		DefaultHttpClient client = new DefaultHttpClient();
+		HttpGet httpGet = new HttpGet(url);
+		JSONObject jsonObject = null;
+		HttpResponse httpResponse = client.execute(httpGet);
+		HttpEntity entity = httpResponse.getEntity();
+		if(entity != null){
+			String result = EntityUtils.toString(entity,"UTF-8");
+			jsonObject = JSONObject.fromObject(result);
+		}
+		return jsonObject;
+	}
+	/**
+	 * 
+	 * @param url
+	 * @param outStr
+	 * @return
+	 * @throws ParseException
+	 * @throws IOException
+	 */
+	public static JSONObject doPostStr(String url,String outStr) throws ParseException, IOException{
+		DefaultHttpClient client = new DefaultHttpClient();
+		HttpPost httpost = new HttpPost(url);
+		JSONObject jsonObject = null;
+		httpost.setEntity(new StringEntity(outStr,"UTF-8"));
+		HttpResponse response = client.execute(httpost);
+		String result = EntityUtils.toString(response.getEntity(),"UTF-8");
+		jsonObject = JSONObject.fromObject(result);
+		return jsonObject;
+	}
 }
